832371cfc1eca810475e948b70ec4c7d6b2ab013
Jakob889 <css_k@hotmail.com>
2020-02-17 15:30:51 +0100

    SQL update error

--- FlightAttendant/FlightAttendant/Program.cs
+++ FlightAttendant/FlightAttendant/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FlightAttendant.Staging;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -18,9 +19,22 @@ namespace FlightAttendant
             Console.WriteLine(obj.GetFlights());
             Console.Read();
 
+            //db load
+            using (var ctx = new FlightAttendantEntities())
+            {
+                var flight1 = new Flights() { FlightNr = "FL1", From = "ZRH", To = "DBU", Seats = 4 };
+                ctx.Flights.Add(flight1);
+                ctx.SaveChanges();
+            }
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm());
+
+
+
+
+
 
 
         }
