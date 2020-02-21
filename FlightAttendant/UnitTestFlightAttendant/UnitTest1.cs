1ccb60c7a45427e804f1ac4201cc16534af49341
Jakob889 <css_k@hotmail.com>
2020-02-21 16:29:24 +0100

    upload

---
+++ FlightAttendant/UnitTestFlightAttendant/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UnitTestFlightAttendant;
+
+namespace UnitTestFlightAttendant
+{
+    [TestClass]
+    public class UnitTestWCF
+    {
+        [TestMethod]
+        public void TestWCFDataLoadFlights()
+        {
+            var flightServiceClient = new FlightServiceReferenceEmirates.FlightServiceClient();
+
+            var flights = flightServiceClient.GetFlights();
+
+            Assert.IsTrue(flightServiceClient.CurrentConnectionCount == 0);
+        }
+    }
+}
